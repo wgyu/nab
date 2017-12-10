444b584799094947e18f8b38ae831512018e6d05
gg wang <gg wang@wg>
2017-12-10 22:07:15 +0800

    test merge by master

--- src/main/java/wgyu/nab/sample/SampleAction.java
+++ src/main/java/wgyu/nab/sample/SampleAction.java
@@ -6,6 +6,8 @@ import nablarch.fw.web.HttpResponse;
 
 public class SampleAction {
 	public HttpResponse index(HttpRequest request, ExecutionContext context) {
+		int aa = "master";
+		context,setRequestScopedVar("aa", aa);
 		
 		return new HttpResponse("/WEB-INF/view/sample/index.jsp");
 	}
